510eb5545bd525d3044be07bde069956d892401e
renato.cruz <renato.cruz@provi.com.br>
2024-01-13 17:42:08 -0300

    refatorando o teste funcional

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table_element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.ID, 'id_list_table'))
+        )
+
+        rows = table_element.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouviu falar de uma nova aplicação online interessante para
         # lista de tarefas. Ela decide verificar sua homepage
@@ -43,17 +51,9 @@ class NewVisitorTest(unittest.TestCase):
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
         # "1: Buy peacock feathers" como item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
-        # inputbox.submit()
-        time.sleep(3)
+        time.sleep(1)
 
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_name('.//tr')
-        table_element = WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located((By.ID, 'id_list_table'))
-        )
-
-        rows = table_element.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
         # item. Ela insere "Use peacock feathers to make a fly"
@@ -62,16 +62,10 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(1)
 
-        # A página é atualizada novamente e agora mostra os dois itens em sua lista
-        table_element = WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located((By.ID, 'id_list_table'))
-        )
-
-        rows = table_element.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota
         # que o site gerou um URL único para ela -- há um pequeno texto
